7540742a2f10717291f1a98966f56fde6e25a315
Aman Raj <amanraj6221@gmail.com>
2025-04-16 22:27:27 +0530

    Updated

--- Assignments/Bg Changer/script.js
+++
@@ -1,7 +0,0 @@
-const colors = ["#3498db", "#2ecc71", "#f39c12", "#9b59b6", "#e74c3c", "#1abc9c", "#34495e"];
-let currentIndex = 0;
-
-function changeBackground() {
-  document.body.style.backgroundColor = colors[currentIndex];
-  currentIndex = (currentIndex + 1) % colors.length;
-}
